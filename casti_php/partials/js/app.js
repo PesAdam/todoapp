4cead7d53485efca84b89da275211606c15e1bc8
kocurik_adam <pesadam15@gmail.com>
2020-09-03 15:46:44 +0200

    opravene linky pri pridani novej polozky

--- casti_php/partials/js/app.js
+++ casti_php/partials/js/app.js
@@ -18,29 +18,30 @@
 
         req.done(function(data){
             if(data === 'funguje'){
-
-                var li = $('<li class="list-group-item">'+ input.val() +'</li>'); 
-
-                li.appendTo('.items')
-                    .css({ backgroundColor: '#00bc8c' })
-                    .delay(250)
-                    .animate ({ backgroundColor: '#353535' });  
+                $.ajax({ url: 'http://localhost/php/toddoappka//index.php'}).done(function(html){
+                    var novy_element = $(html).find('li:last-child');
                     
+                    novy_element.appendTo('.items')
+                        .css({backgroundColor: '#00bc8c'})
+                        .delay(200)
+                        .animate({backgroundColor: '#353535'});
 
-                input.val('').focus();
+                        input.val('').focus();
+                });
+                
             }
             
 
         });
 
-        input.on('keypress', function(event){
+       /* input.on('keypress', function(event){
 
             if( event.which ===  13 ){
                 form.submit();
                 return false;
             }
 
-        });
+        }); */
 
         /* EDIT FORMULAR */
         $('#edit').find('#text').select();
